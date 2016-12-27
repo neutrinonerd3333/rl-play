26b056924842422fc2e2702561d76790b081d670
Tony Zhang <txz@mit.edu>
2016-12-26 17:57:34 -0800

    DeepQNetwork implemented

--- qlearner.py
+++ qlearner.py
@@ -4,8 +4,24 @@ import collections
 import random
 import numpy
 
-import sklearn.neural_network
+# import sklearn.neural_network
 import keras.models
+import keras.layers
+import keras.backend
+import keras.utils.np_utils
+import tensorflow
+
+# LOTS of inspiration from https://github.com/matthiasplappert/keras-rl/
+
+class LearnerMemory:
+    def __init__(self):
+        self.history = []
+
+    def append(self, item):
+        self.history.append(item)
+
+    def sample(self, sample_size):
+        return random.sample(self.history, min(sample_size, len(self.history)))
 
 
 class BaseQApproximator:
@@ -26,13 +42,13 @@ class TabularQApproximator(BaseQApproximator):
     def __init__(self, action_n, batch_size=None):
         self.action_n = action_n
         self.table = collections.defaultdict(lambda: numpy.random.normal(0, 0.1, self.action_n))
-        self.history = []
+        self.history = LearnerMemory()
         self.batch_size = batch_size
 
     def best_action(self, state: str) -> int:
         return numpy.argmax(self.table[state])
 
-    def update(self, old_state, new_state, action, reward, terminal, gamma, learning_rate):
+    def update(self, old_state, new_state, action, reward, terminal, gamma, **kwargs):
         """
         Update our table of Q values with the Bellman equation.
 
@@ -45,6 +61,7 @@ class TabularQApproximator(BaseQApproximator):
                               update step. Must be in [0, 1]
         """
         # precondition
+        learning_rate = kwargs["learning_rate"]
         assert 0 <= learning_rate <= 1
         assert 0 <= gamma < 1
 
@@ -52,12 +69,11 @@ class TabularQApproximator(BaseQApproximator):
         if self.batch_size is not None:
             self.history.append((old_state, new_state, action, reward, terminal))
 
-            experience = random.sample(self.history, min(len(self.history), self.batch_size))
-            olds, acts, rewards, terminalness = [[tup[i] for tup in experience] for i in (0, 2, 3, 4)]
+            experience = self.history.sample(self.batch_size)
+            olds, news, acts, rewards, terminalness = [[tup[i] for tup in experience] for i in range(5)]
 
             old_q = numpy.array([self.table[old][act] for (old, _, act, _, _) in experience])
-
-            expected_futures = gamma * numpy.array([numpy.max(self.table[tup[1]]) for tup in experience])
+            expected_futures = gamma * numpy.array([numpy.max(self.table[new]) for new in news])
             new_q = rewards + numpy.logical_not(terminalness) * expected_futures
 
             updated = (1 - learning_rate) * old_q + learning_rate * new_q
@@ -72,23 +88,91 @@ class TabularQApproximator(BaseQApproximator):
             
             self.table[old_state][action] = (1 - learning_rate) * old_q + learning_rate * new_q
 
-
-class DeepQApproximator(BaseQApproximator):
-    def __init__(self, model):
+class DeepQNetwork(BaseQApproximator):
+    def __init__(self, model, batch_size=None, delta_clip=numpy.inf):
         # self.mlp = sklearn.neural_network.MLPRegressor(*args, **kwargs)
-        self.history = []
+        self.history = LearnerMemory()
+        self.batch_size = batch_size
+        self.delta_clip = delta_clip
+
+        # input model
+        self.model = model
+        inputs = model.inputs
+        outputs = model.outputs
+        y_pred_tensor = outputs[0]
+
+        # target (actor) model
+        config = {
+            'class_name': model.__class__.__name__,
+            'config': model.get_config(),
+        }
+        self.target_model = keras.models.model_from_config(config)
+
+        self.model.compile(optimizer='sgd', loss='mse')
+        self.target_model.compile(optimizer='sgd', loss='mse')
+
+
+        # assert len(outputs) == 1
+        # print(keras.backend.int_shape(y_pred_tensor))
+        # assert len(keras.backend.int_shape(y_pred_tensor)) == 1
+        self.action_n = keras.backend.int_shape(y_pred_tensor)[1]
+        y_true_tensor = keras.layers.Input(name='y_true', shape=(self.action_n,))
+        action_tensor = keras.layers.Input(name='action_mask', shape=(self.action_n,))
+
+        def huber_loss(error, clip):
+            squared_loss = keras.backend.square(error) / 2
+            if numpy.isinf(clip):
+                return squared_loss
+            
+            condition = keras.backend.abs(x) < clip
+            linear_loss = clip * (K.abs(x) - clip / 2)
+
+            return tensorflow.select(condition, squared_loss, linear_loss)
+
+        def huber_loss_function(args):
+            y_true, y_pred, mask = args
+            errors = y_true - y_pred
+            losses = huber_loss(errors, self.delta_clip)
+            return keras.backend.sum(losses * mask, axis=-1)
+
+        def identity(y_true, y_pred):
+            return y_pred
+
+        loss_tensor = keras.layers.Lambda(huber_loss_function, output_shape=(1,), name='loss')([y_true_tensor, y_pred_tensor, action_tensor])
+        self.trainable_model = keras.models.Model(input=(inputs + [y_true_tensor, action_tensor]), output=loss_tensor)
+        self.trainable_model.compile(loss=identity, optimizer='sgd')
 
     def best_action(self, state):
-        pass
+        np_state = numpy.array(state).reshape(1, -1)
+        q_vals = self.target_model.predict(np_state)
+        # print("Q-vals in state {}: {}".format(state, q_vals))
+        # print("best action: {}".format(numpy.argmax(q_vals)))
+        return numpy.argmax(q_vals)
 
-    def update(self, old_state, new_state, action, reward, gamma, minibatch_size):
+    def update(self, old_state, new_state, action, reward, terminal, gamma, **kwargs):
         assert 0 <= gamma < 1
 
         # add to history
-        self.history.append((old_state, action, reward, new_state))
+        self.history.append((old_state, new_state, action, reward, terminal))
+        
+        experience = self.history.sample(self.batch_size)
+        cur_batch_size = len(experience)
+        olds, news, acts, rewards, terminalness = [numpy.array([tup[i] for tup in experience]) for i in range(5)]
 
-        # minibatch gradient descent update
-        minibatch_experience = random.sample(self.history, minibatch_size)
+        discounted_futures = gamma * numpy.max(self.target_model.predict_on_batch(news), axis=1)
+        q_vals = rewards + numpy.logical_not(terminalness) * discounted_futures
+        q_val_array = numpy.zeros((cur_batch_size, self.action_n))
+        for ind in range(cur_batch_size):
+            q_val_array[ind][acts[ind]] = q_vals[ind]
+
+        # update our networks
+        acts_one_hot = keras.utils.np_utils.to_categorical(acts, self.action_n)
+        assert acts_one_hot.shape == (cur_batch_size, self.action_n)
+
+        dummy_targets = numpy.zeros((cur_batch_size, 1))
+
+        self.trainable_model.train_on_batch([olds, q_val_array, acts_one_hot], dummy_targets)
+        self.target_model.set_weights(self.model.get_weights())
 
 
 class QLearner:
@@ -156,7 +240,7 @@ class QLearner:
         @param reward: the reward we got for the action
         @param terminal: whether new_state is a terminal state
         """
-        self.q_approximator.update(old_state, new_state, action, reward, terminal, self.gamma, self.current_learning_rate)
+        self.q_approximator.update(old_state, new_state, action, reward, terminal, self.gamma, learning_rate=self.current_learning_rate)
 
     def decay(self, i_episode):
         self.current_epsilon = max(self.epsilon_min, self.epsilon - i_episode * self.epsilon_decay_rate)
