4f6bc06599b72510280d571e593ee92b1ab9dbb5
Tony Zhang <txz@mit.edu>
2017-01-09 20:32:46 -0500

    Significant revisions to binary heap code
    Added rank-based sampling; wrote tests for partitioning function.

--- binaryheap.py
+++ binaryheap.py
@@ -1,6 +1,9 @@
 import random
 import numpy
 
+import itertools
+import functools
+
 def parent(ind):
     return (ind - 1) // 2
 
@@ -8,58 +11,127 @@ def children(ind):
     left = 2 * ind + 1
     return (left, left + 1)
 
+class HeapLookupEntry:
+    """
+    A barebones implementation of an index-priority tuple for our heap.
+    """
+    def __init__(self, index, priority):
+        self._index = index
+        self._priority = priority
 
-class HeapElement:
-    def __init__(self, name, key):
-        self._name = name
-        self._key = key
+    def index(self):
+        return self._index
 
-    def name(self):
-        return self._name
+    def priority(self):
+        return self._priority
 
-    def key(self):
-        return self._key
+    def set_index(self, index):
+        self._index = index
 
-    def __ge__(self, other):
-        return self._key >= other._key
+    def set_priority(self, priority):
+        self._priority = priority
 
-    def __le__(self, other):
-        return self._key <= other._key
+    def __repr__(self):
+        return str((self._index, self._priority))
+
+
+@functools.lru_cache(maxsize=128, typed=False)
+def rank_based_partition(length: int, bins: int):
+    """
+    Partition the Riemann sum 1/1 + 1/2 + ... + 1/length into approximately
+    equal-size bins, and return a list of the right-endpoints.
+    """
+    assert length >= bins
+
+    probability_normalization = sum(map(lambda x: 1/x, range(1, length + 1)))
+    bin_size = probability_normalization / bins
+
+    partial_sum = 0
+    right_endpoints = []
+    for x in range(1, length + 1):
+        partial_sum += (1 / x)
+        if partial_sum >= bin_size:
+            partial_sum -= bin_size
+            right_endpoints.append(x)
+
+    # handle floating point errors
+    if right_endpoints[-1] != length:
+        right_endpoints.append(length)
+
+    # sanity check
+    if len(right_endpoints) != bins:
+        print(right_endpoints)
+        raise RuntimeError("Length mismatch. length = {}, bins = {}".format(length, bins))
+    return right_endpoints
+
+
+# taken from https://docs.python.org/3/library/itertools.html#recipes
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
 
 class BinaryHeap:
+    """
+    Max-heap implementation.
+    """
+
     def __init__(self):
         self._array = []
+        self._ind_lookup = dict()
 
-    def _compare(self, priority1, priority2):
-        return priority1 >= priority2
+    def _compare(self, item1, item2):
+        items = [item1, item2]
+        priorities = list(map(lambda i: self._ind_lookup[i].priority(), items))
+        return priorities[0] >= priorities[1]
 
     def insert(self, item, priority):
-        self._array.append(HeapElement(item, priority))
-        ind = len(self._array) - 1
+        # should not call insert on things we already have
+        assert item not in self._ind_lookup
+
+        ind = len(self._array)
+        self._ind_lookup[item] = HeapLookupEntry(ind, priority)
+        self._array.append(item)
         self._up_heap(ind)
 
-    def change_priority(self, ind, priority):
-        cur_item = self._array[ind].name()
-        cur_priority = self._array[ind].key()
+    def change_priority(self, item, priority):
+        ind_lookup = self._ind_lookup[item]
+        ind = ind_lookup.index()
+        cur_priority = ind_lookup.priority()
+        assert item == self._array[ind]
+
         if cur_priority == priority:
             return
 
-        self._array[ind] = HeapElement(cur_item, priority)
+        # update priority and maintain heap invariant
+        self._ind_lookup[item].set_priority(priority)
         if cur_priority > priority:
             self._heapify(ind)
         elif cur_priority < priority:
             self._up_heap(ind)
 
     def _up_heap(self, ind):
+        # basecase: root of the heap
         if ind == 0:
             return
 
         parent_ind = parent(ind)
-        cur_elt = self._array[ind]
-        parent_elt = self._array[parent_ind]
-        if not self._compare(parent_elt, cur_elt):
-            self._array[ind] = parent_elt
-            self._array[parent_ind] = cur_elt
+
+        item = self._array[ind]
+        parent_item = self._array[parent_ind]
+
+        # maintain heap invariant if broken
+        if not self._compare(parent_item, item):
+            self._array[ind] = parent_item
+            self._array[parent_ind] = item
+
+            # swap in lookup array
+            self._ind_lookup[item].set_index(parent_ind)
+            self._ind_lookup[parent_item].set_index(ind)
+
+            # recurse
             self._up_heap(parent_ind)
 
     def _heapify(self, ind):
@@ -67,42 +139,61 @@ class BinaryHeap:
         if left >= len(self._array):
             return  # leaf node!
         elif right >= len(self._array):
-            child_ind = right
+            child_ind = left
         else:
             child_ind = left if self._compare(self._array[left], self._array[right]) else right
 
-        cur_elt, child_elt = self._array[ind], self._array[child_ind]
+        item, child_item = self._array[ind], self._array[child_ind]
         # check heap invariant
-        if self._compare(cur_elt, child_elt):
+        if self._compare(item, child_item):
             return
         
-        self._array[ind], self._array[child_ind] = child_elt, cur_elt
+        self._array[ind], self._array[child_ind] = child_item, item
+        self._ind_lookup[item].set_index(child_ind)
+        self._ind_lookup[child_item].set_index(ind)
+
         self._heapify(child_ind)
 
     def trim(self):
-        return self._array.pop()
+        trimmed_item = self._array.pop()
+        del self._ind_lookup[trimmed_item]
+        return trimmed_item
 
     def array(self):
-        return list(map(lambda x: x.name(), self._array))
+        return list(self._array)
 
-    def _heapcheck(self):
-        return all([self._compare(self._array[parent(i)], self._array[i]) for i in range(1, len(self._array))])
+    def _check_rep(self):
+        if len(self._array) != len(self._ind_lookup):
+            raise RuntimeError("Array and lookup table size mismatch")
+        elif any([not self._compare(self._array[parent(i)], self._array[i]) for i in range(1, len(self._array))]):
+            raise RuntimeError("Heap invariant violated! Internal array: {}".format(self._array))
+        elif any([self._ind_lookup[item].index() != index for (index, item) in enumerate(self._array)]):
+            raise RuntimeError("Lookup table corrupted; table: {}; array: {}".format(self._ind_lookup, self._array))
 
     def sort(self):
-        self._array.sort(key=lambda x: -x.key())
+        self._array.sort(key=lambda x: -self._ind_lookup[x].priority())
+        for (ind, item) in enumerate(self._array):
+            self._ind_lookup[item].set_index(ind)
 
     def sample(self, n):
         assert len(self._array) >= n
-        # TODO: prioritize!
-        # TODO continue debugging
-        # python cartpole-run.py --deep --batch-size 32 --gamma 0.9 --gamma-final 0.99 --anneal 100 -v --prioritize
-        return list(map(lambda x: (x[0], x[1].name()), random.sample(enumerate(self._array), n)))
+
+        partitions = pairwise(itertools.chain([0], rank_based_partition(len(self._array), n)))
+        return [random.choice(self._array[left:right])
+            for (left, right) in partitions]
+
+        # indices = [random.choice(range(left, right)) for (left, right) in partitions]
+        # ret = [self._array[ind] for ind in indices]
+        # priorities = [self._ind_lookup[item].priority() for item in ret]
+        # return ret
+
+        # # python cartpole-run.py --deep --batch-size 32 --gamma 0.9 --gamma-final 0.99 --anneal 100 -v --prioritize
 
     def max_priority(self):
         try:
-            return self._array[0].key()
+            return self._ind_lookup[self._array[0]].priority()
         except IndexError:
-            return 1
+            return 1  # if no elements currently in array
 
     def __len__(self):
         return len(self._array)
--- test_binaryheap.py
+++ test_binaryheap.py
@@ -1,6 +1,6 @@
 import unittest
 
-from binaryheap import BinaryHeap
+from binaryheap import BinaryHeap, rank_based_partition
 
 # class Widget:
 # 	def __init__(self, name, key):
@@ -18,6 +18,7 @@ wb = "b"
 wc = "c"
 wd = "d"
 we = "e"
+very_big_number = 987654321
 
 class TestBinaryHeap(unittest.TestCase):
 	def setUp(self):
@@ -26,22 +27,26 @@ class TestBinaryHeap(unittest.TestCase):
 	# insert: 
 	def test_insert(self):
 		self.heap.insert(wa, 100)
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [wa])
 
 	def test_insert_two(self):
 		self.heap.insert(wa, 100)
 		self.heap.insert(wb, 50)
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [wa, wb])
 
 	def test_insert_out_of_order(self):
 		self.heap.insert(wa, 50)
 		self.heap.insert(wb, 100)
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [wb, wa])
 
 	def test_negative_priority(self):
 		custom_heap = BinaryHeap()
 		custom_heap.insert(wa, -50)
 		custom_heap.insert(wb, -100)
+		self.heap._check_rep()
 		self.assertEqual(custom_heap.array(), [wa, wb])
 
 	def test_insert_many(self):
@@ -51,7 +56,7 @@ class TestBinaryHeap(unittest.TestCase):
 		self.heap.insert(wd, 50)
 		self.heap.insert(we, 100)
 		self.assertEqual(set(self.heap.array()), {wa, wb, wc, wd, we})
-		self.assertTrue(self.heap._heapcheck())
+		self.heap._check_rep()
 
 	def test_sort(self):
 		self.heap.insert(wa, -100)
@@ -60,6 +65,7 @@ class TestBinaryHeap(unittest.TestCase):
 		self.heap.insert(wd, -50)
 		self.heap.insert(we, 100)
 		self.heap.sort()
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [we, wb, wc, wd, wa])
 
 	def test_decrease_key(self):
@@ -69,11 +75,16 @@ class TestBinaryHeap(unittest.TestCase):
 		self.heap.insert(wd, -50)
 		self.heap.insert(we, 100)
 		self.heap.sort()
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [we, wb, wc, wd, wa])
-		self.heap.change_priority(1, -2000)
+
+		self.heap.change_priority(wb, -2000)
 		self.assertNotEqual(self.heap.array(), [we, wb, wc, wd, wa])
+		self.heap._check_rep()
+
 		self.heap.sort()
 		self.assertEqual(self.heap.array(), [we, wc, wd, wa, wb])
+		self.heap._check_rep()
 
 	def test_increase_key(self):
 		self.heap.insert(wa, -100)
@@ -82,8 +93,46 @@ class TestBinaryHeap(unittest.TestCase):
 		self.heap.insert(wd, -50)
 		self.heap.insert(we, 100)
 		self.heap.sort()
+		self.heap._check_rep()
 		self.assertEqual(self.heap.array(), [we, wb, wc, wd, wa])
-		self.heap.change_priority(1, 2000)
+
+		self.heap.change_priority(wb, 2000)
 		self.assertNotEqual(self.heap.array(), [we, wb, wc, wd, wa])
+		self.heap._check_rep()
 		self.heap.sort()
 		self.assertEqual(self.heap.array(), [wb, we, wc, wd, wa])
+
+	def test_max_priority(self):
+		self.heap.insert(wa, very_big_number)
+		self.assertEqual(self.heap.max_priority(), very_big_number)
+
+	def test_max_priority_multiple(self):
+		self.heap.insert(wc, 20)
+		self.heap.insert(wa, very_big_number)
+		self.heap.insert(we, -500)
+		self.assertEqual(self.heap.max_priority(), very_big_number)
+
+class TestRankPartition(unittest.TestCase):
+	
+	def test_trivial(self):
+		right_endpoints = rank_based_partition(1, 1)
+		self.assertEqual(right_endpoints, [1])
+
+	def test_one_bin(self):
+		right_endpoints = rank_based_partition(5, 1)
+		self.assertEqual(right_endpoints, [5])
+
+	def test_two_bins(self):
+		right_endpoints = rank_based_partition(3, 2)
+		self.assertEqual(right_endpoints, [1, 3])
+
+	def test_many_bins(self):
+		right_endpoints = rank_based_partition(10, 3)
+		self.assertEqual(right_endpoints, [1, 4, 10])
+
+	def test_too_many_bins_regression(self):
+		"""
+		This is a regression test.
+		"""
+		right_endpoints = rank_based_partition(14, 14)
+		self.assertEqual(right_endpoints, list(range(1, 15)))
